a14225303ea282716e47591effd54816ca9b8f3e
Steven Xu <steven@stevenxu.ca>
2017-11-12 21:53:42 -0800

    Switch to jest.

--- server/connection_test.js
+++ server/connection_test.js
@@ -1,14 +1,10 @@
 const Connection = require('./connection');
 const WebSocket = require('ws');
-const assert = require('assert');
 const { spy } = require('sinon');
 
-describe('Connection', () => {
-  it('Should send a welcome message', () => {
-    const mockSend = spy();
-    new Connection({ send: mockSend });
-    assert.equal(1, mockSend.callCount);
-    assert.equal(
-      '{"topic":"welcome","message":{}}', mockSend.getCall(0).args[0]);
-  });
+test('constructor sends welcome', () => {
+  const mockSend = spy();
+  new Connection({ send: mockSend });
+  expect(mockSend.callCount).toBe(1);
+  expect(mockSend.getCall(0).args[0]).toBe('{"topic":"welcome","message":{}}');
 });
