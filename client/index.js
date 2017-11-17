03c6baece96bff66babfb836ce8804b245e624f3
Steven Xu <steven@stevenxu.ca>
2017-11-16 20:23:34 -0800

    Add some basic client logic to connect to server.

--- client/index.js
+++ client/index.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const ws = new WebSocket('ws://localhost:3000/');
+ws.onmessage = (message) => {
+  let data;
+  try {
+    data = JSON.parse(message.data);
+  } catch(e) {
+    console.error(e);
+    return;
+  }
+  const topic = data['topic'];
+  switch(topic) {
+    case 'welcome':
+      document.getElementById('connecting').style.display = 'none';
+      document.getElementById('done').style.display = '';
+      break;
+    default:
+      console.log(`Unrecognized topic: ${topic}`);
+  }
+};
