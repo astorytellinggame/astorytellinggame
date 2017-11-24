61c4a6a7337390e54fdc2c738e712a0b32791291
Steven Xu <steven@stevenxu.ca>
2017-11-23 21:17:05 -0800

    Added message subscribing logic to Connection.

--- server/connection.js
+++ server/connection.js
@@ -13,7 +13,17 @@ class Connection {
    */
   constructor(ws, lobby) {
     this.ws_ = ws;
+    this.ws_.on('message', this.handleMessage_.bind(this));
+
     this.lobby_ = lobby;
+
+    /**
+     * A map of topics to resolver functions to call with the data when a
+     * message of the matching topic is received.
+     * @private @const {!Map<string,!Array<!Function<!Object>>>}
+     */
+    this.waitedTopics_ = new Map();
+
     this.notify('welcome', {});
   }
 
@@ -26,11 +36,50 @@ class Connection {
 
   /**
    * @param {string} topic
-   * @param {!Object} message
+   * @param {!Object} data
    */
-  notify(topic, message) {
-    this.ws_.send(JSON.stringify({ topic, message }));
+  notify(topic, data) {
+    this.ws_.send(JSON.stringify({ topic, data }));
   }
+
+  /**
+   * Returns a Promise that resolves to the message data when the next message
+   * matching this topic is received.
+   * @param {string} topic
+   * @return {!Promise<!Object>}
+   */
+  waitForTopic(topic) {
+    let subscribers = this.waitedTopics_.get(topic);
+    if (!subscribers) {
+      subscribers = [];
+      this.waitedTopics_.set(topic, subscribers);
+    }
+    return new Promise((resolve) => {
+      subscribers.push(resolve);
+    });
+  }
+
+  /**
+   * Handles a WebSockets message. Parses the message and dispatches it to
+   * relevant subscribers.
+   */
+  handleMessage_(messageStr) {
+    let message;
+    try {
+      message = JSON.parse(messageStr);
+    } catch(e) {
+      if (e instanceof SyntaxError) {
+        console.log(`Failed to parse message: ${messageStr}`);
+        return;
+      }
+      throw e;
+    }
+    const subscribers = this.waitedTopics_.get(message.topic);
+    if (subscribers) {
+      subscribers.forEach((subscriber) => subscriber(message.data));
+      this.waitedTopics_.set(message.topic, []);
+    }
+  };
 }
 
 module.exports = Connection;
--- server/connection_test.js
+++ server/connection_test.js
@@ -4,19 +4,20 @@ jest.mock('./player');
 const MockPlayer = require('./player');
 
 const Connection = require('./connection');
+const assert = require('assert');
 
 describe('with single mock ws', () => {
   const lobby = new MockLobby();
   let ws;
 
   beforeEach(() => {
-    ws = { close: jest.fn(), send: jest.fn() };
+    ws = { close: jest.fn(), on: jest.fn(), send: jest.fn() };
   });
 
   test('constructor sends welcome', () => {
     new Connection(ws, lobby);
     expect(ws.send.mock.calls).toHaveLength(1);
-    expect(ws.send.mock.calls[0][0]).toBe('{"topic":"welcome","message":{}}');
+    expect(ws.send.mock.calls[0][0]).toBe('{"topic":"welcome","data":{}}');
   });
 
   test('detach', () => {
@@ -25,11 +26,62 @@ describe('with single mock ws', () => {
     expect(ws.close.mock.calls).toHaveLength(1);
   });
 
+  describe('waitForTopic', () => {
+    let c;
+    let pretendMessageReceived;
+
+    beforeEach(() => {
+      c = new Connection(ws, lobby);
+      expect(ws.on.mock.calls).toHaveLength(1);
+      pretendMessageReceived = (messageStr) => {
+        ws.on.mock.calls[0][1](messageStr);
+        return Promise.resolve();
+      };
+    });
+
+    test('basic wait', (done) => {
+      c.waitForTopic('foo').then(done);
+      pretendMessageReceived('{"topic":"foo"}');
+    });
+
+    test('ignores unrelated messages', (done) => {
+      c.waitForTopic('foo').then(
+        () => assert.fail('Should not receive unrelated message.'));
+      c.waitForTopic('bar').then(done);
+      pretendMessageReceived('{"topic":"bar"}');
+    });
+
+    test('multiple subscribers', async () => {
+      let count = 0;
+      c.waitForTopic('foo').then(() => count++);
+      c.waitForTopic('foo').then(() => count++);
+      c.waitForTopic('foo').then(() => count++);
+      c.waitForTopic('barbarbar').then(() => count++);
+      c.waitForTopic('foo').then(() => count++);
+      await pretendMessageReceived('{"topic":"foo"}');
+      expect(count).toEqual(4);
+    });
+
+    test('subscribe only once', async () => {
+      let count = 0;
+      c.waitForTopic('foo').then(() => count++);
+      c.waitForTopic('bar').then(() => count++);
+      await pretendMessageReceived('{"topic":"foo"}');
+      expect(count).toEqual(1);
+      await pretendMessageReceived('{"topic":"foo"}');
+      expect(count).toEqual(1);
+      await pretendMessageReceived('{"topic":"bar"}');
+      expect(count).toEqual(2);
+      await pretendMessageReceived('{"topic":"bar"}');
+      expect(count).toEqual(2);
+    });
+  });
+
   test('notify', () => {
     const c = new Connection(ws, lobby);
     c.notify('foo', {'bar': 'baz'});
     expect(ws.send.mock.calls).toHaveLength(2);
     expect(ws.send.mock.calls[1][0])
-      .toBe('{"topic":"foo","message":{"bar":"baz"}}');
+      .toBe('{"topic":"foo","data":{"bar":"baz"}}');
   });
 });
