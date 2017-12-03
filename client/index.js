983790c38d1f9671719387d77cd563acdf66c1c8
Steven Xu <steven@stevenxu.ca>
2017-12-03 15:01:55 -0800

    Upgrade to React.

--- client/index.js
+++ client/index.js
@@ -1,19 +1,5 @@
-const ws = new WebSocket(`ws://${window.location.host}/`);
-ws.onmessage = message => {
-  let data;
-  try {
-    data = JSON.parse(message.data);
-  } catch (e) {
-    console.error(e);
-    return;
-  }
-  const topic = data['topic'];
-  switch (topic) {
-    case 'welcome':
-      document.getElementById('connecting').style.display = 'none';
-      document.getElementById('done').style.display = '';
-      break;
-    default:
-      console.log(`Unrecognized topic: ${topic}`);
-  }
-};
+const Client = require('./client');
+const React = require('react'); // Required for jsx transform.
+const ReactDOM = require('react-dom');
+
+ReactDOM.render(<Client />, document.getElementById('app'));
